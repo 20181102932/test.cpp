7ac55eae4dd7aa791f08d0f344c64bca84185633
s20181102932 <s20181102932@admindeimac-88.local>
2019-06-12 20:23:45 +0800

    Initial Commit

---
+++ test.cpp/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test.cpp
+//
+//  Created by s20181102932 on 2019/6/12.
+//  Copyright © 2019 s20181102932. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
